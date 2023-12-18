56ae7c2fad9ac8ecf2327cc9d305aa57ffc45edf
Gianluca Nuzzaci <gianluca@Gianlucas-Air.lan>
2023-12-18 18:19:07 +0100

    lastoneass

--- project/user.js
+++ project/user.js
@@ -71,7 +71,7 @@ const main = () => {
     const poiDiv = document.querySelectorAll(".poi");
     poiDiv.forEach((div) => {
       div.onclick = () => {
-        let address = "https://nuzzacigianluca.github.io/project/detail.html?poi=0";
+        let address = "https://nuzzacigianluca.github.io/bologna/project/detail.html?poi=0";
         let url = new URL(String(address));
         url.searchParams.set('poi', div.id)
         console.log(url.href);
@@ -166,11 +166,12 @@ const main = () => {
       if (feature.length != 0) {
         array.forEach((element)=>{
           if (element.nome==feature[0].name){
-            let address = "https://nuzzacigianluca.github.io/project/detail.html?poi=0";
+            let address = "https://nuzzacigianluca.github.io/bologna/project/detail.html?poi=0";
             let url = new URL(String(address));
             url.searchParams.set('poi', element.id)
             console.log(url.href);
-            location.href = url.href;
+            location.href = "./detail.html"
+            //location.href = url.href;
             Cookies.set('back','user.html');
           }
         })
